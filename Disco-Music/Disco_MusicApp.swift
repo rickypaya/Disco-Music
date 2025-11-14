981e0194885a8f6b93b1a06ac45349d184b4c598
Ricky Payares <rpayaresswe@gmail.com>
2025-11-13 23:43:27 -0500

    Initial Commit

---
+++ Disco-Music/Disco_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disco_MusicApp.swift
+//  Disco-Music
+//
+//  Created by Ricardo Payares on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Disco_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
